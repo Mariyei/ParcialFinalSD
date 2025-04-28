98569a7819d93891c68b51d3b094ab38aab6e903
MariferAlvarez <88256311+Mariyei@users.noreply.github.com>
2025-04-28 01:23:41 -0600

    Initial project structure and source code added for ATM prototype

---
+++ Inc/LCDC.h
@@ -0,0 +1,18 @@
+/*
+ * LCDC.h
+ *
+ *  Created on: Apr 27, 2025
+ *      Author: Lovem
+ */
+
+#ifndef LCDC_H_
+#define LCDC_H_
+
+
+void LCD_init (void) {
+	PORTS_init();
+
+	delay_ms ()
+}
+
+#endif /* LCDC_H_ */
---
+++ Inc/stm32l053.h
@@ -0,0 +1,13 @@
+/*
+ * stm32l053.h
+ *
+ *  Created on: Apr 27, 2025
+ *      Author: Lovem
+ */
+
+#ifndef INC_STM32L053_H_
+#define INC_STM32L053_H_
+
+
+
+#endif /* INC_STM32L053_H_ */
